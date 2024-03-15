b40707490892ac95c98a1c14d1db8d71dadb229e
jegatheesprana <jegatheesprana@gmail.com>
2024-03-15 22:42:55 +0530

    ixed bug in register page

--- src/sections/register/register-view.tsx
+++ src/sections/register/register-view.tsx
@@ -1,4 +1,14 @@
-import { Alert, Box, Card, Grid, IconButton, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import {
+    Alert,
+    Box,
+    Card,
+    Grid,
+    IconButton,
+    InputAdornment,
+    Stack,
+    TextField,
+    Typography,
+} from '@mui/material';
 import React, { useState } from 'react';
 import Logo from 'src/components/logo';
 import MuiLink from '@mui/material/Link';
@@ -9,9 +19,7 @@ import { bgGradient } from 'src/theme/css';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-
 export default function RegisterView() {
-
     const theme = useTheme();
     const navigate = useNavigate();
 
@@ -20,90 +28,90 @@ export default function RegisterView() {
     const [success, setSuccess] = useState(false);
     const [showPassword, setShowPassword] = useState(false);
 
-
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const data = {
-          firstName: formData.get('firstName'),
-          lastName: formData.get('lastName'),
-          email: formData.get('email'),
-          password: formData.get('password')
+            firstName: formData.get('firstName'),
+            lastName: formData.get('lastName'),
+            email: formData.get('email'),
+            password: formData.get('password'),
         };
-        console.log(data)
+        console.log(data);
         if (data.email && data.password && data.firstName && data.lastName) {
-          setLoading(true);
-          setError('');
-          axios
-            .post(`${import.meta.env.VITE_BASE_URL}/auth/register`, data)
-            .then((response) => {
-              setSuccess(true);
-              setTimeout(() => {
-                navigate('/login');
-              }, 3000);
-            })
-            .catch((err) => {
-              setError(err.response.data.message);
-            })
-            .finally(() => {
-              setLoading(false);
-            });
+            setLoading(true);
+            setError('');
+            axios
+                .post(`${import.meta.env.VITE_BASE_URL}/auth/register`, data)
+                .then(() => {
+                    setSuccess(true);
+                    setTimeout(() => {
+                        navigate('/login');
+                    }, 3000);
+                })
+                .catch((err) => {
+                    setError(err.response.data.message);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
         } else {
-          setError('Please fill the required fields');
+            setError('Please fill the required fields');
         }
-      };
+    };
 
     const renderForm = (
         <Box component="form" noValidate onSubmit={handleSubmit}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField 
-                fullWidth
-                name="email" 
-                label="Email address" />
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        autoComplete="given-name"
+                        name="firstName"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                    />
                 </Grid>
-                  <Grid item xs={12}>
-                <TextField
-                fullWidth
-                    name="password"
-                    label="Password"
-                    type={showPassword ? 'text' : 'password'}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton
-                                    onClick={() => setShowPassword(!showPassword)}
-                                    edge="end"
-                                >
-                                    <Iconify
-                                        icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}
-                                    />
-                                </IconButton>
-                            </InputAdornment>
-                        ),
-                    }}
-                /></Grid></Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="family-name"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth name="email" label="Email address" />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type={showPassword ? 'text' : 'password'}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        edge="end"
+                                    >
+                                        <Iconify
+                                            icon={
+                                                showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'
+                                            }
+                                        />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Grid>
+            </Grid>
 
             {error && (
                 <Alert sx={{ mt: 1 }} severity="error">
@@ -112,7 +120,7 @@ export default function RegisterView() {
             )}
             {success && (
                 <Alert sx={{ mt: 2 }} severity="success">
-                    Logged in
+                    Registered
                 </Alert>
             )}
 
@@ -130,8 +138,8 @@ export default function RegisterView() {
         </Box>
     );
 
-  return (
-    <Box
+    return (
+        <Box
             sx={{
                 ...bgGradient({
                     color: alpha(theme.palette.background.default, 0.9),
@@ -159,13 +167,8 @@ export default function RegisterView() {
                     <Typography variant="h4">Register to CK Produce</Typography>
 
                     <Typography variant="body2" sx={{ mt: 2, mb: 5 }}>
-                    Already have an account?
-                        <MuiLink
-                            component={Link}
-                            to="/login"
-                            variant="subtitle2"
-                            sx={{ ml: 0.5 }}
-                        >
+                        Already have an account?
+                        <MuiLink component={Link} to="/login" variant="subtitle2" sx={{ ml: 0.5 }}>
                             Log in
                         </MuiLink>
                     </Typography>
@@ -173,5 +176,5 @@ export default function RegisterView() {
                 </Card>
             </Stack>
         </Box>
-  )
+    );
 }
